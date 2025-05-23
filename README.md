d01a73a463b36f4c02a1a4f38e7d08a74c55ed11
ishangithub2024 <rudrashis.chowdhury@gmail.com>
2025-05-23 12:24:07 +0530

    new project

---
+++ README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f0273b6af51cdd383d894bf0be5b71d7f320f4eadc8c1aec4616bbc412ff56a7
+size 1090
---
+++ app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c3dc00c35003658584f244c631637af110495c33104653080ca124dacd439e82
+size 2442
